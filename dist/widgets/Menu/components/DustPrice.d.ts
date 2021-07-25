6d3b9ca70d1be17b1d465e9a3a1a9610624c145b
moonwalkerjim <support@moonwalker.network>
2021-07-25 19:28:10 +0100

    initial commit

---
+++ dist/widgets/Menu/components/DustPrice.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+interface Props {
+    dustPriceUsd?: number;
+}
+declare const _default: React.NamedExoticComponent<Props>;
+export default _default;
---
+++ src/widgets/Menu/config.ts
@@ -0,0 +1,137 @@
+export const links = [
+  {
+    label: "Home",
+    icon: "HomeIcon",
+    href: "/",
+  },
+  {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://exchange.moonwalker.network",
+      },
+      {
+        label: "Liquidity",
+        href: "https://exchange.moonwalker.network/#/pool",
+      },
+    ],
+  },
+  {
+    label: "Farms",
+    icon: "FarmIcon",
+    href: "/farms",
+  },
+  {
+    label: "Pools",
+    icon: "PoolIcon",
+    href: "/syrup",
+  },
+  {
+    label: "Lottery",
+    icon: "TicketIcon",
+    href: "/lottery",
+  },
+  {
+    label: "NFT",
+    icon: "NftIcon",
+    href: "/nft",
+  },
+  {
+    label: "Profile & Teams",
+    icon: "GroupsIcon",
+    items: [
+      {
+        label: "Leaderboard",
+        href: "/teams",
+      },
+      {
+        label: "YourProfile",
+        href: "/",
+      },
+    ],
+    calloutClass: "rainbow",
+  },
+  {
+    label: "Info",
+    icon: "InfoIcon",
+    items: [
+      {
+        label: "Overview",
+        href: "https://info.moonwalker.network",
+      },
+      {
+        label: "Tokens",
+        href: "https://info.moonwalker.network/tokens",
+      },
+      {
+        label: "Pairs",
+        href: "https://info.moonwalker.network/pairs",
+      },
+      {
+        label: "Accounts",
+        href: "https://info.moonwalker.network/accounts",
+      },
+    ],
+  },
+  {
+    label: "IFO",
+    icon: "IfoIcon",
+    items: [
+      {
+        label: "Next",
+        href: "/ifo",
+      },
+      {
+        label: "History",
+        href: "/ifo/history",
+      },
+    ],
+  },
+  {
+    label: "More",
+    icon: "MoreIcon",
+    items: [
+      {
+        label: "Voting",
+        href: "https://voting.moonwalker.network",
+      },
+      {
+        label: "Github",
+        href: "https://github.com/Moonwalkerjim",
+      },
+      {
+        label: "Docs",
+        href: "https://swap-docs.moonwalker.network",
+      },
+      {
+        label: "Blog",
+        href: "https://moonwalker.network.medium.com",
+      },
+    ],
+  },
+];
+
+export const socials = [
+  {
+    label: "Telegram",
+    icon: "TelegramIcon",
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/moonwalkerswap",
+      },
+    ],
+  },
+  {
+    label: "Twitter",
+    icon: "TwitterIcon",
+    href: "https://twitter.com/moonwalkerjim",
+  },
+];
+
+export const MENU_HEIGHT = 64;
+export const MENU_ENTRY_HEIGHT = 48;
+export const SIDEBAR_WIDTH_FULL = 240;
+export const SIDEBAR_WIDTH_REDUCED = 56;
